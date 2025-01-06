e6c6a17578c11770ebb2730aaa05efc007a8a180
Nicola Buompane <n.buompane@cineca.it>
2025-01-06 16:25:38 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:progetto_finale_flutter_advanced/page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const HomePage(),
+        '/checkout': (_) => const HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      appBar: appBar(),
+      body: Stack(
+        children: [stackProducts(), floatingActionButton()],
+      ),
+    );
+  }
+
+  AppBar appBar() => AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.grey.shade100,
+        elevation: 0,
+        title: Column(
+          children: [
+            const Text(
+              "Luigi's",
+              style: TextStyle(
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black),
+            ),
+            Text(
+              "Spedizione gratuita per ordini > 50\$",
+              style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey.shade600),
+            ),
+          ],
+        ),
+      );
+
+  Widget stackProducts() => GridView.builder(
+        padding: const EdgeInsets.fromLTRB(16, 32, 16, 95),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 16, mainAxisSpacing: 16),
+        itemCount: 10,
+        itemBuilder: (contex, index) => Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://loremflickr.com/640/480/food"),
+                        fit: BoxFit.cover)),
+              )),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Testo",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                "prezzo",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 13, color: Colors.grey.shade600),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                minWidth: double.infinity,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    side: BorderSide(color: Colors.black12)),
+                child: Text('Aggiungi'),
+              )
+            ],
+          ),
+        ),
+      );
+
+  Widget floatingActionButton() => Positioned(
+        left: 16,
+        right: 16,
+        bottom: 32,
+        child: MaterialButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/checkout');
+          },
+          height: 50,
+          elevation: 0,
+          minWidth: double.infinity,
+          color: Colors.yellow.shade700,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Text("Completa Acquisto (0)"),
+        ),
+      );
+}
